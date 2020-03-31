523e87f67ce335dd4a2876901738837880e433a5
Hang Ho <bemoon0103@gmail.com>
2020-03-30 23:25:04 -0700

    try heroku

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -11,7 +11,7 @@ function routes (app){
    app.get("/api/notes", function(req, res){
      res.json(db)
    })
-
+ 
    app.post("/api/notes", function(req, res){
        var newNote = req.body;
        newNote.id = id++;
